6dc5789032512046e75d7889f60dfafa4827303f
mpm <mpmiszczyk@gmail.com>
2012-11-10 16:40:37 +0100

    Dokładnie to co Garus pisał na fejsie, może ktoś się przejmie i zacznie używać pull requestów. Nie sprawdzałem działania, i nie podoba mi się ten warning z C++standard

--- lab_04/doxy/html/search/related_6f.js
+++ lab_04/doxy/html/search/related_6f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_3c_3c',['operator&lt;&lt;',['../class_angle.html#a7b631e19cf9d6f84f88e6b30877175f2',1,'Angle']]]
+  ['operator_3c_3c',['operator<<',['../class_angle.html#a7b631e19cf9d6f84f88e6b30877175f2',1,'Angle']]]
 ];
