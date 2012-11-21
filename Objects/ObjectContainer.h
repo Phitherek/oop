3667230513af85523d4d13c222de7e2710c16a5f
Phitherek <phitherek@gmail.com>
2012-11-22 00:13:56 +0100

    documented ObjectContainer class

--- Objects/ObjectContainer.h
+++ Objects/ObjectContainer.h
@@ -1,14 +1,21 @@
 #pragma once
 #include "Object.h"
+/// \file ObjectContainer.h
+/// \brief An implementation of the dynamic array for Object objects.
+/// \author Phitherek_
+/// \date 2012
+/// \version 0.1
 
+/// \class ObjectContainer
+/// \brief A class representing a dynamic array of Object objects.
 class ObjectContainer {
 private:
 Object* _tab;
 int _size;
 public:
-ObjectContainer();
-ObjectContainer(ObjectContainer& oc);
-~ObjectContainer();
-ObjectContainer& push(Object o);
-Object pop();
+ObjectContainer(); ///< A constructor.
+ObjectContainer(ObjectContainer& oc); ///< A copy constructor.
+~ObjectContainer(); ///< A destructor.
+ObjectContainer& push(Object o); ///< A function, that adds an Object to the end of array.
+Object pop(); ///< A function that returns and erases an Object from the end of array.
 };
